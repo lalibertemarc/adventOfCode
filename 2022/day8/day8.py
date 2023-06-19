ca5e00c7bd36f08a760358223494ab0daf7ac01e
marclaliberte <mlaliberte@addenergie.com>
2023-06-19 09:38:19 -0400

    first part of day 8

--- 2022/day8/day8.py
+++ 2022/day8/day8.py
@@ -1,3 +1,4 @@
+import numpy as np
 f = open("./input", "r")
 input = f.read().splitlines()
 
@@ -9,51 +10,64 @@ for line in input:
         newRow.append(c)
     table.append(newRow)
 
+table = np.array(table)
 # print(table)
-visibleTrees = 0
 
-for row in range(0, len(table)):
-    for column in range(0, len(table[row])):
-        if (row == 0 or row == len(table)-1):
-            visibleTrees += 1
-            continue
-        if (column == 0 or column == len(table[row])-1):
-            visibleTrees += 1
-            continue
+def computeFirstAnswer():
+    visibleTrees = 0
+    for row in range(0, len(table)):
+        for column in range(0, len(table[row])):
+            currentTree = table[row][column]
+            if (row == 0 or row == len(table)-1):
+                print('adding first or last row', row, column, currentTree)
+                visibleTrees += 1
+                continue
+            if (column == 0 or column == len(table[row])-1):
+                print('adding first or last column', row, column, currentTree)
+                visibleTrees += 1
+                continue
+            
+            # visible from left
+            sawBiggerTree = False
+            for left in reversed(range(0, column)):
+                if (table[row][left] >= currentTree):
+                    sawBiggerTree = True
+                    break
+            if (not sawBiggerTree):
+                print('adding visible from left', row, column, 'tree', currentTree)
+                visibleTrees += 1
+                continue
 
-        currentTree = table[row][column]
+            # visible from right
+            sawBiggerTree = False
+            for right in range(column+1, len(table[row])):
+                if (table[row][right] >= currentTree):
+                    sawBiggerTree = True
+                    break
+            if (not sawBiggerTree):
+                print('adding visible from right', row, column, 'tree', currentTree)
+                visibleTrees += 1
+                continue
 
-        # visible from left
-        for left in reversed(range(0, column)):
-            if (table[row][left] >= currentTree):
-                break
-        if (left == 0):
+            # visible from top
+            sawBiggerTree = False
+            for top in reversed(range(0, row)):
+                if (table[top][column] >= currentTree):
+                    sawBiggerTree = True
+                    break
+            if (not sawBiggerTree):
+                print('adding visible from top', row, column, 'tree', currentTree)
+                visibleTrees += 1
+                continue
 
-            visibleTrees += 1
-            continue
+            # visible from bottom;
+            sawBiggerTree = False
+            for bottom in range(row+1, len(table)):
+                if (table[bottom][column] >= currentTree):
+                    sawBiggerTree = True
+                    break
+            if (not sawBiggerTree):
+                print('adding visible from bottom', row, column, 'tree', currentTree)
+                visibleTrees += 1
+    print(visibleTrees)
 
-        # visible from right
-        for right in range(column+1, len(table[row])):
-            if (table[row][right] >= currentTree):
-                break
-        if (left == len(table[row])-1):
-            visibleTrees += 1
-            continue
-
-        # visible from top
-        for top in reversed(range(0, row)):
-            if (table[top][column] >= currentTree):
-                break
-        if (top == 0):
-            visibleTrees += 1
-            continue
-
-        # visible from bottom;
-        for bottom in range(row+1, len(table)):
-            if (table[bottom][column] >= currentTree):
-                break
-        if (bottom == len(table)-1):
-            print(currentTree, 'is visible from bottom', row, column)
-            visibleTrees += 1
-
-print(visibleTrees)
